650ef28cfa36c8472503cc56981f3b8f70db02d1
doyky006 <dokhai92@gmail.com>
2024-03-26 13:50:17 +1030

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ICTCapstoneProject.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
