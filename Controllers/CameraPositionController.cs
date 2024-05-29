9e79f514df3b8be34f3eeec049655df8cb7eede3
Yu_Nishimura <nisyy003@mymail.unisa.edu.au>
2024-05-29 13:14:28 +0930

    Citing a file upload function developed by Khai in CameraPositionController

--- Controllers/CameraPositionController.cs
+++ Controllers/CameraPositionController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ICTCapstoneProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using CsvHelper;
+using System.Web;
+using System.Reflection.PortableExecutable;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using System.IO;
+using System;
+using System.Globalization;
 
 namespace ICTCapstoneProject.Controllers
 {
     public class CameraPositionController : Controller
     {
-        public IActionResult Index()
+        public class NullableDoubleConverter : DefaultTypeConverter
         {
-            return View();
+            //Returns the initial view for displaying a list of camera position data.
+            [HttpGet]
+            public IActionResult Index(List<CameraPosition> cameraPositions = null)
+            {
+                cameraPositions = cameraPositions == null ? new List<CameraPosition>() : cameraPositions;
+                return View(cameraPositions);
+            }
+
+            //Receives the uploaded CSV file and stores it on the server.
+            [HttpPost]
+            public IActionResult Index(IFormFile file, [FromServices] IWebHostEnvironment hostingEnvironment)
+            {
+
+                #region Upload CameraPosition CSV file
+                string fileName = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+                using (FileStream fileStream = System.IO.File.Create(fileName))
+                {
+                    file.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+
+                var cameraPositions = GetCameraPositionsFromCSV(fileName);
+
+                #endregion
+
+                return Index(cameraPositions);
+            }
+
+            //Reads a CSV file, converts each row into a CameraPosition and returns a list
+            private List<CameraPosition> GetCameraPositionsFromCSV(string filePath)
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    PrepareHeaderForMatch = args => args.Header.ToLower(),
+                    BadDataFound = context => { },
+                };
+
+                List<CameraPosition> cameraPositions = new List<CameraPosition>();
+                using (var reader = new StreamReader(filePath))
+                using (var csv = new CsvReader(reader, config))
+                {
+                    csv.Context.TypeConverterCache.AddConverter<double?>(new NullableDoubleConverter());
+                    cameraPositions = csv.GetRecords<CameraPosition>().ToList();
+                }
+                return cameraPositions;
+            }
         }
     }
 }
